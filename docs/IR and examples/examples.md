09ca3c213bf3b89f6c3a271678916aab346e7976
Nathan Blackburn <nblackburndeveloper@icloud.com>
2026-02-23 20:58:53 +0000

    refactor GoogleTest fixture for all possible CFG shapes implement test suite for computeUseDef

--- docs/IR and examples/examples.md
+++ docs/IR and examples/examples.md
@@ -7,49 +7,66 @@ There are 4 different types of IR program shapes that a CFG solver needs to be a
 // Block A (entry)
 int x = 10;
 int y = 20;
+goto block_B;
 
 // Block B
-int z = x + y;
-if (z == 30)
-    goto block_C;
+block_B:
+    int z = x + y;
+    if (z == 30)
+        goto block_C;
 
 block_C:
     return z;
 ```
 
 ```plaintext
-Block A (entry):
-    %1 = MOV 10
-    %2 = MOV 20
-    JMP B
+INIT_BLOCK:
+    MOV %1, 10
+    MOV %2, 20
+    JMP BLOCK_B
 
-Block B:
-    %3 = ADD %1, %2
-    BEQ %3, 30, C
+BLOCK_B:
+    ADD %3, %1, %2
+    BEQ %3, 30, BLOCK_C
 
-Block C (exit):
+BLOCK_C:
     RET %3
 ```
 
 
 ## Simple Loop
 ```c
-int a = 10, b = 20;
+int a = 0;
+
+main_block:
+    if (a == 40)
+        goto block_C;
+    else
+        goto block_A;
+
 block_A:
     a++;
+    goto main_block;
 
-int c = a + b;
-if (c == 40)
-    goto block_C;
-else
-    goto block_A;
 
 block_C:
-    return c;
+    return a;
 ```
 
 ```plaintext
+INIT_BLOCK:
+    MOV %1, 0
 
+main_block:
+    BEQ %1, 40, BLOCK_C
+    JMP BLOCK_A
+
+BLOCK_A:
+    ADD %1, %1, 1
+    JMP main_block
+
+BLOCK_C:
+    RET %1
 ```
 
 
@@ -76,6 +93,27 @@ ret_block:
 ```
 
 ```plaintext
+INIT_BLOCK:
+    MOV %1, 0
+    MOV %2, 0
+
+OUTER_BLOCK:
+    BEQ %1, 10, INNER_BLOCK
+
+OUTER_BODY:
+    ADD %1, %1, 1
+    JMP OUTER_BLOCK
+
+INNER_BLOCK:
+    BEQ %2, 5, RET_BLOCK
+
+INNER_BODY:
+    ADD %2, %2, 1
+    JMP INNER_BLOCK
+
+RET_BLOCK:
+    ADD %3, %1, %2
+    RET %3
 ```
 
 
@@ -96,5 +134,21 @@ exit:
 ```
 
 ```plaintext
+INIT_BLOCK: 
+    MOV %1, 5
 
+MAIN_BLOCK:
+    BEQ %1, 5, COND_1
+    JMP COND_2
+
+COND_1:
+    MOV %1, 1
+    JMP RET_BLOCK
+
+COND_2:
+    MOV %1, -1
+    JMP RET_BLOCK
+
+RET_BLOCK:
+    RET %1
 ```
--- tests/test_liveness.cpp
+++ tests/test_liveness.cpp
@@ -1,168 +1,273 @@
 #include <gtest/gtest.h>
+
 #include <IR.h>
 #include <Liveness.h>
+#include <IRParser.h>
+#include <CFGConstructor.h>
+#include <Lexer.h>
 
 #include <utility>
 
 class LivenessAnalysisTest : public ::testing::Test {
 protected:
-    /*
-    The test fixture operates on this test IR program:
-    Block A (entry):
-        %1 = MOV 10
-        %2 = MOV 20
-        JMP B
-
-    Block B:
-        %3 = ADD %1, %2
-        BEQ %3, 30, C
-
-    Block C (exit):
-        RET %3
-    */
-    Function f;
+    std::unique_ptr<Function> dagFN, simpleLoopFN, nestedLoopFN, diamondFN;
 
     LivenessAnalysisTest() {
-        f.name = "test_func";
+        std::string straightLineDAGsrc = R"(
+        INIT_BLOCK:
+            MOV %1, 10
+            MOV %2, 20
+            JMP BLOCK_B
 
-        auto entry = std::make_unique<BasicBlock>();
-        entry->id = 1;
-        entry->label = "entry";
+        BLOCK_B:
+            ADD %3, %1, %2
+            BEQ %3, 30, BLOCK_C
 
-        auto B = std::make_unique<BasicBlock>();
-        B->id = 2;
-        B->label = "test_block_B";
+        BLOCK_C:
+            RET %3
+        )";
 
-        auto exit = std::make_unique<BasicBlock>();
-        exit->id = 3;
-        exit->label = "exit_block_C";
+        std::string simpleLoopSrc = R"(
+        INIT_BLOCK:
+            MOV %1, 0
 
-        // %1 = MOV 10
-        Imm i10 = {10};
-        VReg v1 = {1};
-        entry->instrs.push_back(Instruction {
-            .op = Opcode::MOV,
-            .def = v1,
-            .uses = {i10}
-        });
+        main_block:
+            BEQ %1, 40, BLOCK_C
 
-        // %2 = MOV 20
-        Imm i20 = {20};
-        VReg v2 = {2};
-        entry->instrs.push_back(Instruction {
-            .op = Opcode::MOV,
-            .def = v2,
-            .uses = {i20}
-        });
+        BLOCK_A:
+            ADD %1, %1, 1
+            JMP main_block
 
-        // JMP B
-        Operand b_exit = Label{.name="test_block_B"};
-        entry->instrs.push_back(Instruction {
-            .op = Opcode::JMP,
-            .def = std::monostate{},
-            .uses = {b_exit}
-        });
+        BLOCK_C:
+            RET %1
+        )";
 
-        // %3 = ADD %1, %2
-        VReg v3 = {3};
-        B->instrs.push_back(Instruction {
-            .op = Opcode::ADD,
-            .def = v3,
-            .uses = {v1, v2}
-        });
+        std::string nestedLoopSrc = R"(
+        INIT_BLOCK:
+            MOV %1, 0
+            MOV %2, 0
 
-        // BEQ %3, 30, C
-        Imm i30 = {30};
-        Operand c_exit = Label{.name="exit_block_C"};
-        B->instrs.push_back(Instruction {
-            .op = Opcode::BEQ,
-            .def = std::monostate{},
-            .uses = {v3, i30, c_exit}
-        });
+        OUTER_BLOCK:
+            BEQ %1, 10, INNER_BLOCK
 
-        // RET %3
-        exit->instrs.push_back(Instruction {
-            .op = Opcode::RET,
-            .def = std::monostate(),
-            .uses = {v3}
-        });
+        OUTER_BODY:
+            ADD %1, %1, 1
+            JMP OUTER_BLOCK
 
-        entry->succs.push_back(B.get());
-        B->preds.push_back(entry.get());
-        B->succs.push_back(exit.get());
-        exit->preds.push_back(B.get());
-        
-        f.blocks.push_back(std::move(entry));
-        f.blocks.push_back(std::move(B));
-        f.blocks.push_back(std::move(exit));
+        INNER_BLOCK:
+            BEQ %2, 5, RET_BLOCK
+
+        INNER_BODY:
+            ADD %2, %2, 1
+            JMP INNER_BLOCK
+
+        RET_BLOCK:
+            ADD %3, %1, %2
+            RET %3
+        )";
+
+        std::string diamondSrc = R"(
+        INIT_BLOCK: 
+            MOV %1, 5
+
+        MAIN_BLOCK:
+            BEQ %1, 5, COND_1
+            JMP COND_2
+
+        COND_1:
+            MOV %1, 1
+            JMP RET_BLOCK
+
+        COND_2:
+            MOV %1, -1
+            JMP RET_BLOCK
+
+        RET_BLOCK:
+            RET %1
+        )";
+
+        Lexer dagLexer(straightLineDAGsrc);
+        Lexer simpleLoopLexer(simpleLoopSrc);
+        Lexer nestedLoopLexer(nestedLoopSrc);
+        Lexer diamondLexer(diamondSrc);
+
+        IRParser dagParser(dagLexer);
+        IRParser simpleLoopParser(simpleLoopLexer);
+        IRParser nestedLoopParser(nestedLoopLexer);
+        IRParser diamondParser(diamondLexer);
+
+        dagFN = dagParser.parseFunction();
+        simpleLoopFN = simpleLoopParser.parseFunction();
+        nestedLoopFN = nestedLoopParser.parseFunction();
+        diamondFN = diamondParser.parseFunction();
     }
 };
 
-TEST_F(LivenessAnalysisTest, GatherInitialInfo) {
-    LivenessInfo li = computeUseDef(f);
+TEST_F(LivenessAnalysisTest, GatherInitialInfo_DAG) {
+    LivenessInfo li = computeUseDef(*dagFN);
 
-    // Entry Block
-    std::vector<bool> UEVarBlk1 = li.UEVar[1];
-    std::vector<bool> VarKillBlk1 = li.VarKill[1];
+    std::vector<bool> init_block_varkill = li.VarKill[0];
+    std::vector<bool> init_block_uevar = li.UEVar[0];
 
-    ASSERT_EQ(VarKillBlk1.size(), 3);
+    ASSERT_EQ(init_block_varkill.size(), 3);
 
-    EXPECT_TRUE(VarKillBlk1[1]);
-    EXPECT_TRUE(VarKillBlk1[2]);
-    EXPECT_FALSE(VarKillBlk1[0]);
-    EXPECT_FALSE(UEVarBlk1[0]);
+    ASSERT_TRUE(init_block_varkill[1]);
+    ASSERT_TRUE(init_block_varkill[2]);
 
-    // Block B
-    std::vector<bool> UEVarBlk2 = li.UEVar[2];
-    std::vector<bool> VarKillBlk2 = li.VarKill[2];
+    std::vector<bool> block_b_varkill = li.VarKill[1];
+    std::vector<bool> block_b_uevar = li.UEVar[1];
 
-    ASSERT_EQ(UEVarBlk2.size(), 4);
+    ASSERT_EQ(block_b_uevar.size(), 4);
 
-    EXPECT_TRUE(VarKillBlk2[3]);
-    EXPECT_TRUE(UEVarBlk2[1]);
-    EXPECT_TRUE(UEVarBlk2[2]);
-    EXPECT_FALSE(VarKillBlk2[1]);
+    ASSERT_TRUE(block_b_varkill[3]);
 
-    // Exit Block
-    std::vector<bool> UEVarBlk3 = li.UEVar[3];
-    std::vector<bool> VarKillBlk3 = li.VarKill[3];
+    ASSERT_TRUE(block_b_uevar[1]);
+    ASSERT_TRUE(block_b_uevar[2]);
 
-    ASSERT_EQ(UEVarBlk3.size(), 4);
-    EXPECT_TRUE(UEVarBlk3[3]);
-    EXPECT_FALSE(UEVarBlk3[2]);
+    std::vector<bool> block_c_uevar = li.UEVar[2];
+
+    ASSERT_EQ(block_c_uevar.size(), 4);
+
+    ASSERT_TRUE(block_c_uevar[3]);
 }
 
-TEST_F(LivenessAnalysisTest, Analyse) {
+TEST_F(LivenessAnalysisTest, GatherInitialInfo_SimpleLoop) {
+    LivenessInfo li = computeUseDef(*simpleLoopFN);
+
+    // INIT_BLOCK
+    std::vector<bool> init_block_varkill = li.VarKill[0];
+
+    ASSERT_EQ(init_block_varkill.size(), 2);
+
+    ASSERT_TRUE(init_block_varkill[1]);
+
+    // main_block
+    std::vector<bool> main_block_varkill = li.VarKill[1];
+    std::vector<bool> main_block_uevar = li.UEVar[1];
+
+    ASSERT_TRUE(main_block_uevar[1]);
+
+    // BLOCK_A
+    std::vector<bool> block_a_varkill = li.VarKill[2];
+    std::vector<bool> block_a_uevar = li.UEVar[2];
+
+    ASSERT_EQ(main_block_uevar.size(), 2);
+
+    ASSERT_TRUE(block_a_varkill[1]);
+    ASSERT_TRUE(block_a_uevar[1]);
+
+    // BLOCK_C
+    std::vector<bool> block_c_varkill = li.VarKill[3];
+    std::vector<bool> block_c_uevar = li.UEVar[3];
+
+    ASSERT_EQ(block_c_uevar.size(), 2);
+
+    ASSERT_TRUE(block_c_uevar[1]);
+}
+
+TEST_F(LivenessAnalysisTest, GatherInitialInfo_NestedLoop) {
+    LivenessInfo li = computeUseDef(*nestedLoopFN);
+
+    // INIT_BLOCK
+    std::vector<bool> init_block_varkill = li.VarKill[0];
+
+    ASSERT_EQ(init_block_varkill.size(), 3);
+    ASSERT_TRUE(init_block_varkill[1]);
+    ASSERT_TRUE(init_block_varkill[2]);
+
+    // OUTER_BLOCK
+    std::vector<bool> outer_block_uevar = li.UEVar[1];
+
+    ASSERT_EQ(outer_block_uevar.size(), 2);
+    ASSERT_TRUE(outer_block_uevar[1]);
+
+    // OUTER_BODY
+    std::vector<bool> outer_body_uevar = li.UEVar[2];
+    std::vector<bool> outer_body_varkill = li.VarKill[2];
+
+    ASSERT_EQ(outer_body_uevar.size(), 2);
+    ASSERT_EQ(outer_body_varkill.size(), 2);
+    ASSERT_TRUE(outer_body_uevar[1]);
+    ASSERT_TRUE(outer_body_varkill[1]);
+
+    // INNER_BLOCK
+    std::vector<bool> inner_block_uevar = li.UEVar[3];
+
+    ASSERT_EQ(inner_block_uevar.size(), 3);
+    ASSERT_TRUE(inner_block_uevar[2]);
+
+    // INNER_BODY
+    std::vector<bool> inner_body_uevar = li.UEVar[4];
+    std::vector<bool> inner_body_varkill = li.VarKill[4];
+
+    ASSERT_EQ(inner_block_uevar.size(), 3);
+    ASSERT_TRUE(inner_block_uevar[2]);
+    ASSERT_TRUE(inner_body_varkill[2]);
+
+    // RET_BLOCK
+    std::vector<bool> ret_block_uevar = li.UEVar[5];
+    std::vector<bool> ret_block_varkill = li.VarKill[5];
+
+    ASSERT_EQ(ret_block_uevar.size(), 4);
+    ASSERT_TRUE(ret_block_uevar[2]);
+    ASSERT_TRUE(ret_block_uevar[1]);
+    ASSERT_TRUE(ret_block_varkill[3]);
+}
+
+TEST_F(LivenessAnalysisTest, GatherInitialInfo_Diamond) {
+    LivenessInfo li = computeUseDef(*diamondFN);
+
+    // INIT_BLOCK
+    std::vector<bool> init_block_varkill = li.VarKill[0];
+
+    ASSERT_EQ(init_block_varkill.size(), 2);
+    ASSERT_TRUE(init_block_varkill[1]);
+
+    // MAIN_BLOCK
+    std::vector<bool> outer_block_uevar = li.UEVar[1];
+
+    ASSERT_EQ(outer_block_uevar.size(), 2);
+    ASSERT_TRUE(outer_block_uevar[1]);
+
+    // COND_1
+    std::vector<bool> cond_1_varkill = li.VarKill[2];
+
+    ASSERT_EQ(cond_1_varkill.size(), 2);
+    ASSERT_TRUE(cond_1_varkill[1]);
+
+    // COND_2
+    std::vector<bool> cond_2_varkill = li.VarKill[3];
+
+    ASSERT_EQ(cond_2_varkill.size(), 2);
+    ASSERT_TRUE(cond_2_varkill[1]);
+
+    // RET_BLOCK
+    std::vector<bool> ret_block_uevar = li.UEVar[4];
+
+    ASSERT_EQ(ret_block_uevar.size(), 2);
+    ASSERT_TRUE(ret_block_uevar[1]);
+}
+
+TEST_F(LivenessAnalysisTest, Analyse_DAG) {
     LivenessAnalysis la;
-    LivenessResult c = la.analyse(f);
+    LivenessResult c = la.analyse(*dagFN);
 
-    // Block A (Entry)
-    std::set<int> BlockA_LiveOut = c.liveoutSet[1];
-    std::set<int> BlockA_LiveIn = c.liveinSet[1];
+}
 
-    ASSERT_EQ(BlockA_LiveOut.size(), 2);
-    ASSERT_EQ(BlockA_LiveIn.size(), 0);
+TEST_F(LivenessAnalysisTest, Analyse_SimpleLoop) {
+    LivenessAnalysis la;
+    LivenessResult c = la.analyse(*dagFN);
 
-    ASSERT_TRUE(BlockA_LiveOut.contains(1));
-    ASSERT_TRUE(BlockA_LiveOut.contains(2));
+}
 
-    // Block B
-    std::set<int> BlockB_LiveOut = c.liveoutSet[2];
-    std::set<int> BlockB_LiveIn = c.liveinSet[2];
+TEST_F(LivenessAnalysisTest, Analyse_NestedLoop) {
+    LivenessAnalysis la;
+    LivenessResult c = la.analyse(*dagFN);
 
-    ASSERT_EQ(BlockB_LiveOut.size(), 1);
-    ASSERT_EQ(BlockB_LiveIn.size(), 2);
+}
 
-    ASSERT_TRUE(BlockB_LiveIn.contains(1));
-    ASSERT_TRUE(BlockB_LiveIn.contains(2));
-    ASSERT_TRUE(BlockB_LiveOut.contains(3));
+TEST_F(LivenessAnalysisTest, Analyse_Diamond) {
+    LivenessAnalysis la;
+    LivenessResult c = la.analyse(*dagFN);
 
-    // Block C (Exit)
-    std::set<int> BlockC_LiveOut = c.liveoutSet[3];
-    std::set<int> BlockC_LiveIn = c.liveinSet[3];
-
-    ASSERT_EQ(BlockC_LiveOut.size(), 0);
-    ASSERT_EQ(BlockC_LiveIn.size(), 1);
-
-    ASSERT_TRUE(BlockC_LiveIn.contains(3));
 }
